8fb9ca2a2f9307c0a47136197a599f8620092787
Abhilaksh Bansal <abhilakshbansal2001@gmail.com>
2022-05-25 00:43:13 +0530

    Added css cache

--- public/worker.js
+++ public/worker.js
@@ -5,6 +5,8 @@ var urlsToCache = [
   // '/static/js/main.chunk.js',
   '/static/js/main.7591caf0.chunk.js',
   '/static/js/2.f12ecddc.chunk.js',
+  '/static/css/main.2d406175.chunk.css',
+  '/static/css/2.d5c3ed63.chunk.css',
   '/index.html',
   '/home',
   '/category',
